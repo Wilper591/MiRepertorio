3ba0e63d717ca02fd6d5fb561886c667ffd4b315
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-04-19 00:44:24 -0400

    cargando env

--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ import pg from "pg";
 const { Pool } = pg;
 
 const config = {
-  user: "wilper591",
-  host: "dpg-cog0f9ol5elc73dm5o80-a.oregon-postgres.render.com",
-  database: "repertorio_x86u",
-  password: "9sN5Ew742epPIWcwZ23JcIxRKAoYUumD",
-  port: 5432,
-  ssl: true
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  ssl: process.env.SSL,
 };
 
 export const pool = new Pool(config);
